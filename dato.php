014857fef59741369babfec3225226cb3765a684
JFAquinoPando <pandolcik@gmail.com>
2024-09-21 11:52:38 -0400

    ver databases

--- dato.php
+++ dato.php
@@ -1,2 +1,7 @@
 <?php
-    phpinfo();
+    //phpinfo();
+    $conexion = new mysqli("localhost", "root", "password", "sys");
+    $sql = "SELECT * FROM host_summary";
+    $res = $conexion->query($sql);
+    $fila = $res->fetch_assoc();
+    print_r($fila);
